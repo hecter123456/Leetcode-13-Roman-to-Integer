219dde8aea3d6382f13e70221acd5f87c95fc3c5
hecter <hecter654321@gmail.com>
2018-02-24 23:17:39 +0800

    pass testcase : testMinusCase by using record last number

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -9,6 +9,10 @@ class unitest(unittest.TestCase):
         Input = "DCXXI"
         Output = 621
         self.assertEqual(Solution().romanToInt(Input),Output)
+    def testMinusCase(self):
+        Input = "MCMLIV"
+        Output = 1954
+        self.assertEqual(Solution().romanToInt(Input),Output)
 
 class Solution():
     def romanToInt(self, s):
@@ -20,10 +24,12 @@ class Solution():
             "C": 100,
             "D": 500,
             "M":1000}
-        if s == "":
-            return 0
         ans = 0
+        last = 0
         for item in s:
+            if last < dic[item]:
+                ans -= last*2
+            last = dic[item]
             ans += dic[item]
         return ans
 
