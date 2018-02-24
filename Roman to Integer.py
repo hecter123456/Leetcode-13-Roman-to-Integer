6f7738d52b2506990198519df36e8ef97584b9dc
hecter <hecter654321@gmail.com>
2018-02-24 23:08:48 +0800

    pass testcase: testSample by using dictionary

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -5,11 +5,27 @@ class unitest(unittest.TestCase):
         Input = ""
         Output = 0
         self.assertEqual(Solution().romanToInt(Input),Output)
+    def testSample(self):
+        Input = "DCXXI"
+        Output = 621
+        self.assertEqual(Solution().romanToInt(Input),Output)
 
 class Solution():
     def romanToInt(self, s):
+        dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M":1000}
         if s == "":
             return 0
+        ans = 0
+        for item in s:
+            ans += dic[item]
+        return ans
 
 if __name__ == '__main__':
     unittest.main()
